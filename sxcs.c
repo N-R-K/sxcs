de9e407d40140f090d2f13851dffdbb692d19195
NRK <nrk@disroot.org>
2023-05-30 02:49:37 +0600

    sort includes

--- sxcs.c
+++ sxcs.c
@@ -18,12 +18,12 @@
  */
 
 #include <errno.h>
+#include <poll.h>
+#include <signal.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdarg.h>
-#include <poll.h>
-#include <signal.h>
 
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
